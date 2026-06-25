fb1d1730f702e440a59d6e059b3a3c569967c2e1
Kunal Patil <kunal.patil.ve@mitwpu.edu.in>
2026-06-25 17:55:10 +0530

    add data prepocessing_obj

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import numpy
+import numpy as np
 import pandas as pd
 from sklearn.impute import KNNImputer
 from sklearn.pipeline import Pipeline
@@ -15,3 +15,74 @@ from networksecurity.entity.artifac_entity import (
 from networksecurity.entity.entity_config import DataTransformationConfig, DataValidationConfig
 from networksecurity.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.utils.main_utils.utils import save_numpy_array_data
+
+
+class DataTransformation:
+    def __init__(
+        self,
+        data_transformation_config: DataTransformationConfig,
+        data_validation_artifact: DataValidationArtifact,
+    ):
+        try:
+            self.data_transformation_config: DataTransformationConfig = data_transformation_config
+            self.data_validation_artifact: DataValidationArtifact = data_validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e   
+        
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+    
+    def get_data_transformer_object(self) -> Pipeline:
+        logging.info("Creating data transformer object")
+        try:
+            imputer: KNNImputer = KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS)
+            pipeline = Pipeline(steps=[("imputer", imputer)])
+            processor: Pipeline = pipeline([("imputer", imputer)])
+            return processor
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+        
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+        logging.info("Starting data transformation")
+        try:
+            logging.info("Reading valid train and test data")
+            train_data = self.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_data = self.read_data(self.data_validation_artifact.valid_test_file_path)
+
+             # training data frame
+            input_feature_train_df = train_data.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_train_df = train_data[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+
+            # testing data frame
+            input_feature_test_df = test_data.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_test_df = test_data[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
+
+            logging.info("Obtaining preprocessing object")
+            preprocessore = self.get_data_transformer_object()
+
+            logging.info("Transforming training and testing data using preprocessing object")
+            prepocessing_obj = preprocessore.fit(input_feature_train_df)
+            input_feature_train_arr = prepocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = prepocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("Saving transformed training and testing data")
+            # save numpy array
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, test_arr)
+            save_object(self.data_transformation_config.transformed_object_file_path, prepocessing_obj)
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+    
+    
+       
